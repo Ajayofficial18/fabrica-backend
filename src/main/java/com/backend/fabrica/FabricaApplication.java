882294fb29221bf67a76d991f6db43b63720e9a6
Ajay <ajgour14@gmail.com>
2024-10-16 11:04:40 +0530

    setting up boiler project

---
+++ src/main/java/com/backend/fabrica/FabricaApplication.java
@@ -0,0 +1,15 @@
+package com.backend.fabrica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaApplication.class, args);
+		System.out.println("======started=========");
+		
+	}
+
+}
